6645583c82b16a3f50f6afe28d4072a3c3482a5c
Joshua Fogg <joshua.h.fogg@gmail.com>
2024-08-08 22:38:07 +0100

    Removed license boilerplate

--- Analysis/analysis.sh
+++ Analysis/analysis.sh
@@ -2,20 +2,6 @@
 
 # This is a quick script which cleans up the data recieved from the bot
 # so that it can be graphed easily by R.
-# Copyright (C) 2014  Joshua Fogg
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 # Fetches data
 cp ../Bot/output.dat output.dat
